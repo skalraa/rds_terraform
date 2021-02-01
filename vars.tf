f080b587311c9f1e3f83b92e323302db689c179a
skalraa <63340289+skalraa@users.noreply.github.com>
2021-02-01 18:04:46 +0530

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,15 @@
+variable "db_instance" {
+  default = "db.t2.micro"
+}
+
+variable "rds_subnet1" {
+  default = "subnet-0da176fb16dfe5ee6"
+}
+
+variable "rds_subnet2" {
+  default = "subnet-0f50dc34aba1f2090"
+}
+
+variable "vpc_id" {
+  default = "vpc-0b8b0ab40d3b2869c"
+}
